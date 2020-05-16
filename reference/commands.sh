83ed2a6eac1a5589587d03caca355fd5c3f41d75
pradeepreddydevops84 <pradeepreddydevops84@gmail.com>
2020-05-16 21:23:57 +0530

    1st commit

---
+++ reference/commands.sh
@@ -0,0 +1,16 @@
+
+
+1. Logged user:-
+
+# $ whoami  -->  to display username
+[npr@vm22entos7 ~]$ whoami
+npr
+
+# $ w --> to display logged in users info and uptime, load avg etc
+[npr@vm22entos7 ~]$ w
+ 21:15:44 up 8 min,  2 users,  load average: 0.00, 0.08, 0.07
+USER     TTY      FROM             LOGIN@   IDLE   JCPU   PCPU WHAT
+pradeep  tty1                      21:15    8.00s  0.02s  0.02s -bash
+npr      pts/0    gateway          21:11    0.00s  0.10s  0.05s w
+
+
